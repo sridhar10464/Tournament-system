56b1221a0d7d246e9146c60bfd3598bd568fc372
Sridhar N <110921859+sridhar10464@users.noreply.github.com>
2023-05-28 10:50:02 +0530

    Init commit

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,8 +5,7 @@ const Home = () => {
     <div className="home">
       <h2>Welcome to the Tournament System!</h2>
       <p>In this app backend is performed well but frontend has some problem
-        in retreiving data from backend due to some fetching errors, try to fix this errors but time is running up,
-      at last minute submit this project</p>
+        in retreiving data from backend due to some fetching errors.</p>
       {/* Add your home content here */}
     </div>
   );
