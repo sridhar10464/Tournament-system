ce976de41ccd7a824c2ddcfd13e126762f469732
Sridhar N <110921859+sridhar10464@users.noreply.github.com>
2023-05-28 11:16:28 +0530

    Init commit

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,7 +6,11 @@ const Home = () => {
       <h2>Welcome to the Tournament System!</h2>
       <p>In this app backend is performed well but frontend has some problem
         in retreiving data from backend due to some fetching errors.</p>
-      {/* Add your home content here */}
+    
+      <h5>Backend and Frontend Source code: "https://github.com/sridhar10464/Tournament-system.git"</h5>
+      <h5>Frontend deployed URL: "https://tournament-system-oveo.onrender.com"</h5>
+      <h5>Backend deployed URL: "https://tournament-system.onrender.com "</h5>
+     
     </div>
   );
 };
